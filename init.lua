844582212fc4077181b3a227fb646b56e69c01e0
rkeenan <ross.keenan@protonmail.comm>
2024-03-23 11:37:32 -0500

    updated settings in init.lua. added vim-visual-multi.

--- init.lua
+++ init.lua
@@ -102,7 +102,10 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, for help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
+
+-- treat word like "hello-world" as one word
+vim.opt.iskeyword:append { '-', '_' }
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
---
+++ lua/custom/plugins/vim-visual-multi.lua
@@ -0,0 +1,10 @@
+-- File: lua/custom/plugins/nvim-visual-multi.lua
+
+return {
+  {
+    'mg979/vim-visual-multi',
+    branch = 'master',
+    config = function() end,
+    event = 'User AstroFile',
+  },
+}
