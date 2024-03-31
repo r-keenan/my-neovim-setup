6f0ea94bcff7c41411cc5084e4ecf4d6f87ae7e2
rkeenan <ross.keenan@protonmail.comm>
2024-03-31 18:17:15 -0500

    updated neotree plugin with options.

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -9,6 +9,24 @@ return {
     'MunifTanjim/nui.nvim',
   },
   config = function()
-    require('neo-tree').setup {}
+    require('neo-tree').setup {
+      filesystem = {
+        filtered_items = {
+          visible = true, -- when true, they will just be displayed differently than normal items
+          hide_dotfiles = false,
+          hide_hidden = true,
+          hide_gitignored = false,
+          always_show = { -- remains visible even if other settings would normally hide it
+            '.vscode',
+            '.jenkinsfiles',
+          },
+          never_show = {
+            '.git',
+            '.github',
+          },
+        },
+      },
+    }
   end,
+  vim.keymap.set('n', '<leader>e', '<Cmd>Neotree toggle<CR>', { desc = 'Toggle file explorer' }),
 }
