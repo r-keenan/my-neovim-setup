c836808e134bbcaf57314e5020dc643666fd68b3
rkeenan <ross.keenan@protonmail.comm>
2024-04-18 10:36:23 -0500

    added rainbow brackets without additional customization.

---
+++ lua/custom/plugins/rainbow-delimiters.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    'HiPhish/rainbow-delimiters.nvim',
+    event = 'VeryLazy',
+    --[[
+    config = function()
+      -- This module contains a number of default definitions
+      local rainbow_delimiters = require 'rainbow-delimiters'
+
+      ---@type rainbow_delimiters.config
+      vim.g.rainbow_delimiters = {
+        -- try typing in here
+      }
+    end,
+    ]]
+    --
+  },
+}
