8711012100c26e80747822969a965bdd3af5b3c2
rkeenan <ross.keenan@protonmail.comm>
2024-05-05 18:46:05 -0500

    added friendly-snippets

--- init.lua
+++ init.lua
@@ -711,12 +711,11 @@ require('lazy').setup({
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
-          {
-            'rafamadriz/friendly-snippets',
-            config = function()
-              require('luasnip.loaders.from_vscode').lazy_load()
-            end,
-          },
+          --{
+          -- config = function()
+          -- require('luasnip.loaders.from_vscode').lazy_load()
+          -- end,
+          --},
         },
       },
       'saadparwaiz1/cmp_luasnip',
@@ -726,6 +725,7 @@ require('lazy').setup({
       --  into multiple repos for maintenance purposes.
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
+      'rafamadriz/friendly-snippets',
     },
     config = function()
       -- See `:help cmp`
