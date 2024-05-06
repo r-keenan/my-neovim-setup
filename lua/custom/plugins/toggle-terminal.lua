215c8e41cfdf9f97f437613ddf0a1f9da448e077
rkeenan <ross.keenan@protonmail.comm>
2024-05-06 18:34:53 -0500

    added toggleterm to plugins.

---
+++ lua/custom/plugins/toggle-terminal.lua
@@ -0,0 +1,8 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = '*',
+  keys = {
+    { '<leader>tt', '<cmd>ToggleTerm size=40 dir=~/Desktop direction=horizontal<cr>', desc = 'Open a horizontal terminal at the Desktop directory' },
+  },
+  config = true,
+}
