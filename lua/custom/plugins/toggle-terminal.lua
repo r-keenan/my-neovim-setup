69302fb77c0d7553d9ff9bdc829d59d044656217
rkeenan <ross.keenan@protonmail.comm>
2024-05-08 20:28:09 -0500

    added a few config options. removed setting default dir to desktop.

--- lua/custom/plugins/toggle-terminal.lua
+++ lua/custom/plugins/toggle-terminal.lua
@@ -2,11 +2,13 @@ return {
   'akinsho/toggleterm.nvim',
   version = '*',
   keys = {
-    { '<leader>tt', '<cmd>ToggleTerm size=10 dir=~/Desktop direction=horizontal<cr>', desc = 'Open a horizontal terminal at the Desktop directory' },
+    { '<leader>tt', '<cmd>ToggleTerm size=10 direction=horizontal<cr>', desc = 'Open a horizontal terminal at the Desktop directory' },
   },
   config = function()
     require('toggleterm').setup {
       autochdir = true,
+      start_in_insert = true,
+      close_on_exit = true,
       size = 5,
       float_ops = {
         height = 5,
