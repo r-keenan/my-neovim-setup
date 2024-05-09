3d03810d666e0a362a7ef11cac201fd7eace5fa3
rkeenan <ross.keenan@protonmail.comm>
2024-05-08 19:57:57 -0500

    Added default terminal height when using keyboard shortcut to open toggleterm. also set pwd for directory when openining it.

--- lua/custom/plugins/toggle-terminal.lua
+++ lua/custom/plugins/toggle-terminal.lua
@@ -2,7 +2,15 @@ return {
   'akinsho/toggleterm.nvim',
   version = '*',
   keys = {
-    { '<leader>tt', '<cmd>ToggleTerm size=40 dir=~/Desktop direction=horizontal<cr>', desc = 'Open a horizontal terminal at the Desktop directory' },
+    { '<leader>tt', '<cmd>ToggleTerm size=10 dir=~/Desktop direction=horizontal<cr>', desc = 'Open a horizontal terminal at the Desktop directory' },
   },
-  config = true,
+  config = function()
+    require('toggleterm').setup {
+      autochdir = true,
+      size = 5,
+      float_ops = {
+        height = 5,
+      },
+    }
+  end,
 }
