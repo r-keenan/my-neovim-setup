73b3eafe12464fccf24bfaab30da558bd4068663
r-keenan <ross.keenan@protonmail.comm>
2024-06-09 09:14:37 -0500

    Increased height of toggle term window

--- lua/custom/plugins/toggle-terminal.lua
+++ lua/custom/plugins/toggle-terminal.lua
@@ -2,16 +2,16 @@ return {
   'akinsho/toggleterm.nvim',
   version = '*',
   keys = {
-    { '<leader>tt', '<cmd>ToggleTerm size=10 direction=horizontal<cr>', desc = 'Open a horizontal terminal at the Desktop directory' },
+    { '<leader>tt', '<cmd>ToggleTerm size=20 direction=horizontal<cr>', desc = 'Open a horizontal terminal at the Desktop directory' },
   },
   config = function()
     require('toggleterm').setup {
       autochdir = true,
       start_in_insert = true,
       close_on_exit = true,
-      size = 10,
+      size = 20,
       float_ops = {
-        height = 10,
+        height = 20,
       },
     }
   end,
