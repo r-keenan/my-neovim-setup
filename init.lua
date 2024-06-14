6d4dcfda1b49e89659b1cc7068ab123bf545ba6b
r-keenan <ross.keenan@protonmail.comm>
2024-06-13 20:41:54 -0500

    added omnisharp for c# development

--- init.lua
+++ init.lua
@@ -591,6 +591,7 @@ require('lazy').setup({
         prismals = {},
         tailwindcss = {},
         elixirls = {},
+        omnisharp = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
