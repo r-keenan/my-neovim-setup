9e016aef6091882c077401a888a0b6620d6e5aec
r-keenan <ross.keenan@protonmail.com>
2024-08-07 09:04:18 -0500

    fixed issued where space key was not working in Terminal mode.

--- init.lua
+++ init.lua
@@ -175,7 +175,7 @@ vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Adding keymap for toggling close toggletermina. this command is used to exit from terminal mode into normal mode.l
-vim.keymap.set('t', '<leader>tt', '<C-\\><C-n>:CFloatTerm<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>tt', '<C-\\><C-n>:CFloatTerm<CR>', { noremap = true, silent = true })
 
 -- Use 'j + j' to exit Insert mode instead of Esc key
 local options = { noremap = true }
