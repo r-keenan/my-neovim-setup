85ad1a5d41fae76df34d3642a9239ab67278b4d0
r-keenan <ross.keenan@protonmail.comm>
2024-10-01 20:14:09 -0500

    added keymap for opening GrugFar

--- lua/custom/plugins/grug-far.lua
+++ lua/custom/plugins/grug-far.lua
@@ -1,6 +1,12 @@
 return {
   'MagicDuck/grug-far.nvim',
   config = function()
-    require('grug-far').setup {}
+    local gf = require 'grug-far'
+
+    gf.setup()
+
+    vim.keymap.set('n', '<leader>gf', function()
+      gf.open()
+    end)
   end,
 }
