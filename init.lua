bdcf44f4fc16e37ef6dbc92d74e224d473c96559
r-keenan <ross.keenan@protonmail.comm>
2024-10-01 19:56:41 -0500

    Removed tsserver, because it is deprecated. replaced with ts_ls

--- init.lua
+++ init.lua
@@ -610,7 +610,7 @@ require('lazy').setup({
       local servers = {
         gopls = {},
         svelte = {},
-        tsserver = {},
+        ts_ls = {},
         pyright = {},
         omnisharp = {},
         csharp_ls = {},
@@ -628,8 +628,7 @@ require('lazy').setup({
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
+        -- But for many setups, the LSP (`ts_ls`) will work just fine
         --
 
         lua_ls = {
@@ -670,6 +669,7 @@ require('lazy').setup({
         'eslint_d',
         'yamllint',
         'csharpier',
+        'ts_ls',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -679,7 +679,7 @@ require('lazy').setup({
             local server = servers[server_name] or {}
             -- This handles overriding only values explicitly passed
             -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for tsserver)
+            -- certain features of an LSP (for example, turning off formatting for ts_ls)
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
             require('lspconfig')[server_name].setup(server)
           end,
