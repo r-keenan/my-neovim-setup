8a40755676dea35123d884995f5f7507f00ec0c5
r-keenan <ross.keenan@protonmail.com>
2024-10-28 10:48:07 -0500

    Added CodeCompanion setting anthropic as default LLM and then used 1password CLI to store the Anthropic API key.

---
+++ lua/custom/plugins/codecompanion.lua
@@ -0,0 +1,48 @@
+return {
+  'olimorris/codecompanion.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-treesitter/nvim-treesitter',
+    'hrsh7th/nvim-cmp', -- Optional: For using slash commands and variables in the chat buffer
+    'nvim-telescope/telescope.nvim', -- Optional: For using slash commands
+    { 'stevearc/dressing.nvim', opts = {} }, -- Optional: Improves `vim.ui.select`
+  },
+  config = function()
+    require('codecompanion').setup {
+      strategies = {
+        chat = {
+          adapter = 'anthropic',
+        },
+        inline = {
+          adapter = 'anthropic',
+        },
+        agent = {
+          adapter = 'anthropic',
+        },
+      },
+      adapters = {
+        anthropic = function()
+          return require('codecompanion.adapters').extend('anthropic', {
+            env = {
+              api_key = 'cmd:op read op://personal/Anthropic/credential --no-newline',
+            },
+          })
+        end,
+      },
+    }
+  end,
+
+  vim.keymap.set('n', '<leader>cb', '<cmd>CodeCompanionActions<cr>', { noremap = true, silent = true, desc = 'CodeCompanionActions' }),
+  vim.api.nvim_set_keymap('v', '<leader>cb', '<cmd>CodeCompanionActions<cr>', { noremap = true, silent = true, desc = 'CodeCompanionActions' }),
+  vim.api.nvim_set_keymap('n', '<leader>cc', '<cmd>CodeCompanionChat Toggle<cr>', { noremap = true, silent = true, desc = 'toggle CodeCompanionChat window' }),
+  vim.api.nvim_set_keymap('v', '<leader>cc', '<cmd>CodeCompanionChat Toggle<cr>', { noremap = true, silent = true, desc = 'toggle CodeCompanionChat window' }),
+  vim.api.nvim_set_keymap(
+    'v',
+    '<leader>ac',
+    '<cmd>CodeCompanionChat Add<cr>',
+    { noremap = true, silent = true, desc = 'Add code to CodeCompanionChat window' }
+  ),
+
+  -- Expand 'cc' into 'CodeCompanion' in the command line
+  vim.cmd [[cab cc CodeCompanion]],
+}
