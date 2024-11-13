d770e8eb9b12720212d43e2c1e1a65e7c4bf0f6d
r-keenan <ross.keenan@protonmail.com>
2024-11-13 10:02:34 -0600

    added plugin for better markdown rendering

--- lua/custom/plugins/codecompanion.lua
+++ lua/custom/plugins/codecompanion.lua
@@ -5,6 +5,7 @@ return {
     'nvim-treesitter/nvim-treesitter',
     'hrsh7th/nvim-cmp', -- Optional: For using slash commands and variables in the chat buffer
     'nvim-telescope/telescope.nvim', -- Optional: For using slash commands
+    { 'MeanderingProgrammer/render-markdown.nvim', ft = { 'markdown', 'codecompanion' } }, -- Optional: For prettier markdown rendering
     { 'stevearc/dressing.nvim', opts = {} }, -- Optional: Improves `vim.ui.select`
   },
   config = function()
