44facdce1726adeb214d8f9b476c8448bd9a8a00
r-keenan <ross.keenan@protonmail.com>
2024-11-12 19:19:07 -0600

    disabled treesj because it is never used. disabled autocompletion for supermaven.

--- lua/custom/plugins/supermaven.lua
+++ lua/custom/plugins/supermaven.lua
@@ -1,6 +1,10 @@
 return {
   'supermaven-inc/supermaven-nvim',
   config = function()
-    require('supermaven-nvim').setup {}
+    require('supermaven-nvim').setup {
+      disable_inline_completion = true,
+      disable_autocomplete = true,
+      disable_autoformat = true,
+    }
   end,
 }
