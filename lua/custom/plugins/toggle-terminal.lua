11f459603bf95b09fcded3351dc52637640f7e30
r-keenan <ross.keenan@protonmail.com>
2024-11-25 20:13:59 -0600

    Added dooing for todos. updated height of toggle terminal.

--- lua/custom/plugins/toggle-terminal.lua
+++ lua/custom/plugins/toggle-terminal.lua
@@ -2,7 +2,7 @@ return {
   'akinsho/toggleterm.nvim',
   version = '*',
   keys = {
-    { '<leader>tt', '<cmd>ToggleTerm size=20 direction=horizontal<cr>', desc = 'Open a horizontal terminal at the Desktop directory' },
+    { '<leader>tt', '<cmd>ToggleTerm size=10 direction=horizontal<cr>', desc = 'Open a horizontal terminal at the Desktop directory' },
   },
   config = function()
     require('toggleterm').setup {
