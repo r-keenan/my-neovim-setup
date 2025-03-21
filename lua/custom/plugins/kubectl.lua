ad6f066d87e4c458d48faf111f392c45967860bd
r-keenan <ross.keenan@protonmail.comm>
2025-03-20 19:50:17 -0500

    added keybindings for kubectl plugin

--- lua/custom/plugins/kubectl.lua
+++ lua/custom/plugins/kubectl.lua
@@ -1,8 +1,12 @@
 return {
-  {
-    'ramilito/kubectl.nvim',
-    config = function()
-      require('kubectl').setup()
-    end,
+  'Ramilito/kubectl.nvim',
+  opts = {},
+  cmd = { 'Kubectl', 'Kubectx', 'Kubens' },
+  keys = {
+    { '<leader>K', '<cmd>lua require("kubectl").toggle()<cr>' },
+    { '<C-K>', '<Plug>(kubectl.kill)', ft = 'k8s_*' },
+    { '7', '<Plug>(kubectl.view_nodes)', ft = 'k8s_*' },
+    { '8', '<Plug>(kubectl.view_overview)', ft = 'k8s_*' },
+    { '<C-T>', '<Plug>(kubectl.view_top)', ft = 'k8s_*' },
   },
 }
