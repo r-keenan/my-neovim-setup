527cee6965194029cd8c7b3c6fdae337df2ca1bf
r-keenan <ross.keenan@protonmail.comm>
2025-04-02 18:39:43 -0500

    switched out tokyonight for solarized

--- init.lua
+++ init.lua
@@ -1012,13 +1012,14 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-    'folke/tokyonight.nvim',
+    'maxmx03/solarized.nvim',
     priority = 1000, -- make sure to load this before all the other start plugins
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.o.background = 'dark'
+      vim.cmd.colorscheme 'solarized'
 
       -- You can configure highlights by doing something like
       vim.cmd.hi 'Comment gui=none'
