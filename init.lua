2f17346dd73b9ee589df2ed4b1235dca40fe976a
r-keenan <ross.keenan@protonmail.com>
2025-04-13 21:00:51 -0500

    switiched to theme "darkblue." it is kind of retro looking, and i like it.

--- init.lua
+++ init.lua
@@ -1018,7 +1018,7 @@ require('lazy').setup({
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       vim.o.background = 'dark'
-      vim.cmd.colorscheme 'solarized'
+      vim.cmd.colorscheme 'darkblue'
 
       -- You can configure highlights by doing something like
       vim.cmd.hi 'Comment gui=none'
