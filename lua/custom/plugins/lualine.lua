8f3375a5b737575ab58288f83727615cfe12d2e9
r-keenan <ross.keenan@protonmail.com>
2025-04-17 08:56:28 -0500

    changed vim lsp function. previous function is now deprecated.

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -106,7 +106,7 @@ return {
     }
 
     local function getLspName()
-      local buf_clients = vim.lsp.buf_get_clients()
+      local buf_clients = vim.lsp.get_clients()
       local buf_ft = vim.bo.filetype
       if next(buf_clients) == nil then
         return '  No servers'
