827a835502655dcd8360e7667c1317f63958ea7a
r-keenan <ross.keenan@protonmail.com>
2025-04-22 21:17:20 -0500

    removed dark/light mode toggle

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -173,7 +173,6 @@ return {
         Snacks.toggle.diagnostics():map '<leader>ud'
         Snacks.toggle.option('conceallevel', { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map '<leader>uc'
         Snacks.toggle.treesitter():map '<leader>uT'
-        Snacks.toggle.option('background', { off = 'light', on = 'dark', name = 'Dark Background' }):map '<leader>ub'
         Snacks.toggle.inlay_hints():map '<leader>uh'
         Snacks.toggle.indent():map '<leader>ug'
         Snacks.toggle.dim():map '<leader>uD'
