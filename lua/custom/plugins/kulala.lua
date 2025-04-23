5fe8839f915510b918c6a8d45607c2eea304ea70
r-keenan <ross.keenan@protonmail.com>
2025-04-22 21:01:09 -0500

    updated keymaps to the original ones.

--- lua/custom/plugins/kulala.lua
+++ lua/custom/plugins/kulala.lua
@@ -2,7 +2,7 @@ return {
   'mistweaverco/kulala.nvim',
   keys = {
     { '<leader>Rs', desc = 'Send request' },
-    { '<leader>sa', desc = 'Send all requests' },
+    { '<leader>Ra', desc = 'Send all requests' },
     { '<leader>sp', desc = 'Open scratchpad' },
   },
   ft = { 'http', 'rest' },
