836aad25e632986d4925874302c1d61ece8d67e8
r-keenan <ross.keenan@protonmail.com>
2025-06-04 21:59:29 -0500

    added language server for azure pipelines. removed php ls

--- init.lua
+++ init.lua
@@ -727,7 +727,7 @@ require('lazy').setup({
         elixirls = {},
         emmet_language_server = {},
         terraformls = {},
-        intelephense = {},
+        azure_pipelines_ls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -776,7 +776,6 @@ require('lazy').setup({
         'csharpier',
         'ts_ls',
         'lemminx',
-        'intelephense',
         'vimls',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
