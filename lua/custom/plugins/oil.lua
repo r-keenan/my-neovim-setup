401dc099a8de75f8c1e3947014a42c6025d4d814
r-keenan <ross.keenan@protonmail.com>
2025-07-07 12:54:31 -0500

    adjusted max width of oil.nvim float window to prevent space being wasted

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -12,15 +12,28 @@ return {
         view_options = {
           show_hidden = true,
         },
+        float = {
+          -- Padding around the floating window
+          padding = 2,
+          -- max_width and max_height can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+          max_width = 0.5,
+          max_height = 0,
+          border = 'rounded',
+          win_options = {
+            winblend = 0,
+          },
+          -- optionally override the oil buffers window title with custom function: fun(winid: integer): string
+          get_win_title = nil,
+          -- preview_split: Split direction: "auto", "left", "right", "above", "below".
+          preview_split = 'auto',
+          -- This is the config that will be passed to nvim_open_win.
+          -- Change values here to customize the layout
+          override = function(conf)
+            return conf
+          end,
+        },
         preview = {
-          -- Width dimensions can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
-          -- min_width and max_width can be a single value or a list of mixed integer/float types.
-          -- max_width = {100, 0.8} means "the lesser of 100 columns or 80% of total"
-          max_width = 0.9,
-          -- min_width = {40, 0.4} means "the greater of 40 columns or 40% of total"
-          min_width = { 40, 0.4 },
-          -- optionally define an integer/float for the exact width of the preview window
-          width = nil,
+          width = 0.4,
           -- Height dimensions can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
           -- min_height and max_height can be a single value or a list of mixed integer/float types.
           -- max_height = {80, 0.9} means "the lesser of 80 columns or 90% of total"
