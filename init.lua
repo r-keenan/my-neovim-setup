5227c6c51021e13c9a47b0a618ba6230b5435408
r-keenan <ross.keenan@protonmail.com>
2025-07-07 19:49:31 -0500

    added fix for not copying to linux keyboard from neovim

--- init.lua
+++ init.lua
@@ -126,9 +126,22 @@ vim.opt.showmode = false
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.schedule(function()
+if vim.fn.has 'wsl' == 1 then
+  vim.g.clipboard = {
+    name = 'WslClipboard',
+    copy = {
+      ['+'] = 'clip.exe',
+      ['*'] = 'clip.exe',
+    },
+    paste = {
+      ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
+else
   vim.opt.clipboard = 'unnamedplus'
-end)
+end
 
 -- Enable break indent
 vim.opt.breakindent = true
