6c6d8fb2876c581d0c3553a693e879aa78baad77
r-keenan <ross.keenan@protonmail.com>
2025-07-22 14:10:08 -0500

    removed easy-dotnet because i am not using it

--- lua/custom/plugins/easy-dotnet.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    'GustavEikaas/easy-dotnet.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
-    config = function()
-      require('easy-dotnet').setup {}
-    end,
-  },
-}
