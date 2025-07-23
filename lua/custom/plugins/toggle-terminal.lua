2b4dac502a315ca6a3bba55f3188c6218bc9556f
r-keenan <ross.keenan@protonmail.com>
2025-07-23 09:19:28 -0500

    added powershell to be the default on windows

--- lua/custom/plugins/toggle-terminal.lua
+++ lua/custom/plugins/toggle-terminal.lua
@@ -1,3 +1,17 @@
+-- set default terminal to powershell. Windows sets cmd as default otherwise.
+local powershell_options = {
+  shell = vim.fn.executable 'pwsh' == 1 and 'pwsh' or 'powershell',
+  shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;',
+  shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait',
+  shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode',
+  shellquote = '',
+  shellxquote = '',
+}
+
+for option, value in pairs(powershell_options) do
+  vim.opt[option] = value
+end
+
 return {
   'akinsho/toggleterm.nvim',
   version = '*',
