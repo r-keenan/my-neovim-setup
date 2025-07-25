a6f760e1131861ef2cb25fee2c611154d430c666
r-keenan <ross.keenan@protonmail.com>
2025-07-25 13:22:16 -0500

    enabled debugger

--- init.lua
+++ init.lua
@@ -1135,7 +1135,7 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
