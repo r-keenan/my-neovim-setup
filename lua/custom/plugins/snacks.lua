b4ba7b6167c34cff78d26f41dd51ea8f6d59f8d9
r-keenan <ross.keenan@protonmail.com>
2025-07-28 12:43:17 -0500

    removed image, because it is causing issues on windows.

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -12,15 +12,6 @@ return {
       enabled = true,
       sections = {
         {
-          section = 'terminal',
-          cmd = 'cat /tmp/nvim_dashboard_image.txt 2>/dev/null || (chafa '
-            .. imagePath
-            .. ' -f symbols --size 60x30--stretch | tee /tmp/nvim_dashboard_image.txt); sleep 0.2',
-          height = 30,
-          padding = 3,
-        },
-        {
-          pane = 2,
           { section = 'keys', gap = 1, padding = 1 },
           { icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
           { icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
@@ -123,7 +114,6 @@ return {
   end,
   init = function()
     -- Preload the chafa command and save to a temporary file
-    vim.fn.jobstart('chafa ' .. imagePath .. ' -f symbols --size 60x30 --stretch > /tmp/nvim_dashboard_image.txt', { detach = true })
     vim.api.nvim_create_autocmd('User', {
       pattern = 'VeryLazy',
       callback = function()
