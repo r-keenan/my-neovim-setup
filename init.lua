f56f8b43b0547b6680758680c298f82b511188cb
r-keenan <ross.keenan@protonmail.com>
2025-07-29 12:37:35 -0500

    allowed cycling through options with up and down arrow key.

--- init.lua
+++ init.lua
@@ -928,6 +928,8 @@ require('lazy').setup({
       -- Custom keymaps to match nvim-cmp behavior
       ['<C-n>'] = { 'select_next', 'fallback' },
       ['<C-p>'] = { 'select_prev', 'fallback' },
+      ['<Up>'] = { 'select_prev', 'fallback' },
+      ['<Down>'] = { 'select_next', 'fallback' },
       ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
       ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
       ['<C-Space>'] = { 'show', 'show_documentation', 'hide_documentation' },
