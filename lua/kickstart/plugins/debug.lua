b45a5c5b26fbe2c4617af1a3562fe1405b0ef672
r-keenan <ross.keenan@protonmail.com>
2025-08-04 15:13:30 -0500

    got it working

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -1,3 +1,20 @@
+local function rebuild_project(co, path)
+  local spinner = require('easy-dotnet.ui-modules.spinner').new()
+  spinner:start_spinner 'Building'
+  vim.fn.jobstart(string.format('dotnet build %s', path), {
+    on_exit = function(_, return_code)
+      if return_code == 0 then
+        spinner:stop_spinner 'Built successfully'
+      else
+        spinner:stop_spinner('Build failed with exit code ' .. return_code, vim.log.levels.ERROR)
+        error 'Build failed'
+      end
+      coroutine.resume(co)
+    end,
+  })
+  coroutine.yield()
+end
+
 return {
   -- NOTE: Yes, you can install new plugins here!
   'mfussenegger/nvim-dap',
@@ -15,7 +32,7 @@ return {
     -- Go
     'leoluz/nvim-dap-go',
     -- C#/.Net
-    'NicholasMata/nvim-dap-cs',
+    'GustavEikaas/easy-dotnet.nvim',
   },
   keys = {
     -- Basic debugging keymaps, feel free to change to your liking!
@@ -66,6 +83,66 @@ return {
   config = function()
     local dap = require 'dap'
     local dapui = require 'dapui'
+    -- .NET specific setup using `easy-dotnet`
+    local ok, netcoredbg = pcall(require, 'easy-dotnet.netcoredbg')
+    if ok and netcoredbg.register_dap_variables_viewer then
+      netcoredbg.register_dap_variables_viewer()
+    end
+    local dotnet = require 'easy-dotnet'
+
+    local function ensure_dll()
+      if debug_dll ~= nil then
+        return debug_dll
+      end
+      local dll = dotnet.get_debug_dll(true)
+      debug_dll = dll
+      return dll
+    end
+
+    for _, value in ipairs { 'cs', 'fsharp' } do
+      dap.configurations[value] = {
+        {
+          type = 'coreclr',
+          name = 'Program',
+          request = 'launch',
+          env = function()
+            local dll = ensure_dll()
+            local vars = dotnet.get_environment_variables(dll.project_name, dll.relative_project_path)
+            return vars or nil
+          end,
+          program = function()
+            local dll = ensure_dll()
+            local co = coroutine.running()
+            rebuild_project(co, dll.project_path)
+            return dll.relative_dll_path
+          end,
+          cwd = function()
+            local dll = ensure_dll()
+            return dll.relative_project_path
+          end,
+        },
+        {
+          type = 'coreclr',
+          name = 'Test',
+          request = 'attach',
+          processId = function()
+            local res = require('easy-dotnet').experimental.start_debugging_test_project()
+            return res.process_id
+          end,
+        },
+      }
+    end
+
+    -- Reset debug_dll after each terminated session
+    dap.listeners.before['event_terminated']['easy-dotnet'] = function()
+      debug_dll = nil
+    end
+
+    dap.adapters.coreclr = {
+      type = 'executable',
+      command = 'netcoredbg',
+      args = { '--interpreter=vscode' },
+    }
 
     require('mason-nvim-dap').setup {
       automatic_installation = true,
@@ -74,12 +151,6 @@ return {
         'delve',
         'netcoredbg',
       },
-    }
-
-    dap.adapters.coreclr = {
-      type = 'executable',
-      command = vim.fn.exepath 'netcoredbg', -- This finds netcoredbg in PATH
-      args = { '--interpreter=vscode' },
     }
 
     -- Dap UI setup
@@ -127,6 +198,5 @@ return {
         detached = vim.fn.has 'win32' == 0,
       },
     }
-    require('dap-cs').setup()
   end,
 }
