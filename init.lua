f2ddea607ebe1437b20ced6ad29a0e7dcb89277c
r-keenan <ross.keenan@protonmail.com>
2025-08-12 07:24:13 -0500

    added opts to guess-indent

--- init.lua
+++ init.lua
@@ -288,7 +288,7 @@ rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
-  'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+  { 'NMAC427/guess-indent.nvim', opts = {} }, -- Detect tabstop and shiftwidth automatically
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
