c2259d3e1a4f5d43f5774a54bcfb2ee06e6662ef
r-keenan <ross.keenan@protonmail.com>
2025-08-23 17:43:16 -0500

    added autotag closer plugin and updated config for blink.cmp

--- init.lua
+++ init.lua
@@ -936,10 +936,12 @@ require('lazy').setup({
           auto_show = true,
           auto_show_delay_ms = 500,
         },
-        draw = {
-          columns = {
-            { 'label', 'label_description', gap = 1 },
-            { 'kind_icon', 'kind' },
+        menu = {
+          draw = {
+            columns = {
+              { 'label', 'label_description', gap = 1 },
+              { 'kind_icon', 'kind' },
+            },
           },
         },
       },
