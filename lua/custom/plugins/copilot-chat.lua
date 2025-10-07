afbb514aab61649321424e695a1f667332e9475b
r-keenan <ross.keenan@protonmail.com>
2025-10-07 14:09:45 -0500

    added copilot chat

---
+++ lua/custom/plugins/copilot-chat.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    dependencies = {
+      { 'nvim-lua/plenary.nvim', branch = 'master' },
+    },
+    build = 'make tiktoken',
+    opts = {},
+  },
+}
